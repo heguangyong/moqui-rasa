1ab7b430f9d08c4d4c87aac93c4d60a3d1d97295
heguangyong <heguangyong@gmail.com>
2024-05-06 16:05:09 +0800

    update doc

--- actions/actions.py
+++ actions/actions.py
@@ -16,10 +16,7 @@ from rasa_sdk.events import SlotSet
 import requests
 from requests.exceptions import HTTPError
 
-BASE_URL = "http://worldtimeapi.org/api/timezone/"
-
-
-class ActionShowTimeZone(Action):
+class ActionShowUserInfo(Action):
     """
     every class has just 2 methods: name & run
     """
@@ -30,42 +27,16 @@ class ActionShowTimeZone(Action):
         Returns:
             Text: the text we will register in `domain.py`
         """
-        return "action_find_timezone"
+        return "action_find_userinfo"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         # we can get values from slots by `tracker` object
-        target_timezone = tracker.get_slot('target_timezone')
-        try:
-            res = requests.get(BASE_URL + target_timezone)
-            res = res.json()
+        target_userinfo = tracker.get_slot('target_userinfo')
 
-            if res['utc_offset']:
-                output = f"Time zone is {res['utc_offset']}"
-            else:
-                output = "Please type in this structure: Area/Region"
-        except:
-            output = 'Ops! There are too many requests on the time zone API. Please try a few moments later...'
-        dispatcher.utter_message(text=output)
-        return []
-
-
-class ActionQueryUser(Action):
-    def name(self) -> Text:
-        return "action_query_user"
-
-    def run(
-            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
-    ) -> List[Dict]:
-        user_id = tracker.get_slot("user_id")
-
-        if not user_id:
-            dispatcher.utter_message("请输入用户ID或用户名。")
-            return self._clear_slot_values()
-
-        api_url = 'https://d.upservce.com/rest/s1/moqui/users'
+        api_url = "https://d.upservce.com/rest/s1/moqui/users"
         auth_token = 'Basic a2FybWE6QWJjZEAxMjM0'
         headers = {
             'Authorization': auth_token,
@@ -73,48 +44,16 @@ class ActionQueryUser(Action):
         }
 
         try:
-            response = requests.get(f'{api_url}/{user_id}', headers=headers)
-            response.raise_for_status()
-            user_json = response.json()
+            res = requests.get(f'{api_url}/{target_userinfo}', headers=headers)
+            res = res.json()
 
-            if user_json:
-                self._utter_user_details(dispatcher, user_json)
-                return self._set_slot_values(tracker, user_json)
+            if res:
+                output = f"User info is {res}"
             else:
-                dispatcher.utter_message("未找到用户信息。")
-                return self._clear_slot_values()
-
+                output = "Please type in this structure: ID， for example:100000。"
         except HTTPError as e:
-            dispatcher.utter_message(f"发生HTTP错误: {str(e)}")
+            output = f"发生HTTP错误: {str(e)}"
         except Exception as e:
-            dispatcher.utter_message(f"发生错误: {str(e)}")
-
+            output = f"发生错误: {str(e)}"
+        dispatcher.utter_message(text=output)
         return []
-
-    def _utter_user_details(self, dispatcher: CollectingDispatcher, user_json: Dict) -> None:
-        dispatcher.utter_message(
-            template="utter_user_details",
-            user_id=user_json.get('userId', ''),
-            username=user_json.get('username', ''),
-            full_name=user_json.get('userFullName', ''),
-            email=user_json.get('emailAddress', '')
-        )
-
-    def _set_slot_values(self, tracker: Tracker, user_json: Dict) -> List[Dict]:
-        slots_to_set = {
-            "user_id": user_json.get('userId', ''),
-            "username": user_json.get('username', ''),
-            "full_name": user_json.get('userFullName', ''),
-            "email": user_json.get('emailAddress', '')
-        }
-
-        # Check if slots are already set
-        current_slot_values = {slot: tracker.get_slot(slot) for slot in slots_to_set.keys()}
-
-        if current_slot_values != slots_to_set:
-            return [SlotSet(slot, value) for slot, value in slots_to_set.items()]
-        else:
-            return []
-
-    def _clear_slot_values(self) -> List[Dict]:
-        return [SlotSet(slot, None) for slot in ["user_id", "username", "full_name", "email"]]
